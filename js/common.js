b2ac1eb2202f4f97b7503b0e826bdb3865a59244
HelenFly <olalada@i.ua>
2015-03-11 14:04:37 +0200

    kill "wow" js for mobile

--- js/common.js
+++ js/common.js
@@ -7,52 +7,69 @@ $(document).ready(function () {
         }
     });//scroll
     $(window).scroll(function () {
-            if ($(this).scrollTop() > 300) {
-                $('.wrapper').addClass("f_scroll");
-            } else if ($(this).scrollTop() <= 300 && $('.wrapper').hasClass("f_scroll")) {
-                $('.wrapper').removeClass("f_scroll");
-            }
-        });//scroll
+        if ($(this).scrollTop() > 300) {
+            $('.wrapper').addClass("f_scroll");
+        } else if ($(this).scrollTop() <= 300 && $('.wrapper').hasClass("f_scroll")) {
+            $('.wrapper').removeClass("f_scroll");
+        }
+    });//scroll
     $(window).scroll(function () {
-                if ($(this).scrollTop() > 500) {
-                    $('.wrapper').addClass("s_scroll");
-                } else if ($(this).scrollTop() <= 500 && $('.wrapper').hasClass("s_scroll")) {
-                    $('.wrapper').removeClass("s_scroll");
-                }
-            });//scroll
+        if ($(this).scrollTop() > 500) {
+            $('.wrapper').addClass("s_scroll");
+        } else if ($(this).scrollTop() <= 500 && $('.wrapper').hasClass("s_scroll")) {
+            $('.wrapper').removeClass("s_scroll");
+        }
+    });//scroll
     $(window).scroll(function () {
-                    if ($(this).scrollTop() > 75) {
-                        $('.wrapper').addClass("z_scroll");
-                    } else if ($(this).scrollTop() <= 75 && $('.wrapper').hasClass("z_scroll")) {
-                        $('.wrapper').removeClass("z_scroll");
-                    }
-                });//scroll
+        if ($(this).scrollTop() > 75) {
+            $('.wrapper').addClass("z_scroll");
+        } else if ($(this).scrollTop() <= 75 && $('.wrapper').hasClass("z_scroll")) {
+            $('.wrapper').removeClass("z_scroll");
+        }
+    });//scroll
 
-});
+   new $.scrolldeck({
+        buttons: '.nav-button',
+        slides: '.slide',
+        duration: 600,
+        easing: 'easeInOutExpo',
+        offset: 0
+    });
 
-$(document).ready(function() {
-  var deck = new $.scrolldeck({
-    buttons: '.nav-button',
-    slides: '.slide',
-    duration: 600,
-    easing: 'easeInOutExpo',
-	offset: 0
-  });
-});
-
-$(document).ready(function(){
-    $(".btn-slide").click(function(){
+    $(".btn-slide").click(function () {
 //        $(".menu").slideToggle("slow");
         $(".nav_up").toggleClass("nav");
         $(this).toggleClass("active");
     });
 
-});
 
-$(document).ready(function(){
-    $(".nav-button").click(function(){
+    $(".nav-button").click(function () {
         $(".nav_up.nav").removeClass("nav");
         $(".btn-slide").removeClass("active");
     });
+
+
+    if (!isMobile.any) {
+
+        var wow = new WOW(
+            {
+                boxClass: 'wow',      // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset: 0,          // distance to the element when triggering the animation (default is 0)
+                mobile: false,       // trigger animations on mobile devices (default is true)
+                live: true,       // act on asynchronously loaded content (default is true)
+                callback: function (box) {
+                    // the callback is fired every time an animation is started
+                    // the argument that is passed in is the DOM node being animated
+                }
+            }
+        );
+        wow.init();
+    }
+
+    $(".menu-opener").click(function () {
+        $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+    });
+
 
 });
